26840b62db3e910317ca637eea9c8e3e0bc99b75
Husain Alnahash <hussainsaleh180@gmail.com>
2025-10-06 21:01:30 +0300

    reached the frontend, only routing is left

--- src/main.jsx
+++ src/main.jsx
@@ -3,13 +3,8 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './pages/App/App';
 
-import { BrowserRouter as Router } from 'react-router-dom';
-
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
+ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <Router>
-      <App />
-    </Router>
-  </React.StrictMode>
-);
+    <App />
+  </React.StrictMode>,
+)
