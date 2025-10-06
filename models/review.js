e4e8094be23f6c0bb3323d5f457650e11f940330
Husain Alnahash <hussainsaleh180@gmail.com>
2025-10-06 15:36:30 +0300

    added the coding to the right file

---
+++ models/review.js
@@ -0,0 +1,36 @@
+//start by importing the mongoose library
+import mongoose from 'mongoose';
+
+const reviewSchema = new mongoose.Schema({
+  // User's name, can be anonymous if not provided
+  name: { type: String, required: false, default: 'Anonymous' },
+
+  // Email of the user, default to 'Anonymous user' if not provided
+  email: { type: String, required: false, default: 'Anonymous user' },
+
+  // Category for the review (e.g., "service", "product", etc.)
+  category: { 
+    type: String, 
+    required: true, 
+    enum: ['Web Development','UI/UX Design','Content Creation'], // Predefined categories
+    message: '{VALUE} is not a valid category'  // Custom error message
+  },
+
+  // Rating out of 5
+  rating: { 
+    type: Number, 
+    required: true, 
+    min: 1, 
+    max: 5, 
+    default: 5  // Default rating is 5, but user can provide their own
+  }
+}, {
+  // Timestamps will automatically add createdAt and updatedAt fields
+  timestamps: true
+});
+
+
+// Creating the model from the schema
+const Review = mongoose.model('Review', reviewSchema);
+
+export default Review;
