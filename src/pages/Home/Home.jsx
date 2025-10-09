c60edf6f8376b80e8e6570c51e1eaeca7d4d40f7
Husain Alnahash <hussainsaleh180@gmail.com>
2025-10-10 00:37:26 +0300

    the homepage welcome section is somewhat responsive with few problems

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,11 +1,27 @@
 // src/pages/Home/Home.jsx
 import React from 'react';
+import styles from '../Home/Home.module.scss'
 
 const Home = () => {
   return (
-    <div>
-      <h1>Welcome to the Home Page</h1>
-    </div>
+    <main>
+      <section className={styles.welcomeSection}>
+        <div className={styles.left}>
+          <p className={styles.welcome}>Welcome to Storm Lab!</p>
+          <h3 className={styles.deliver}>We deliver solutions through code.</h3>
+           <div className={styles.buttons}>
+            <button className={styles.contactButton}>Contact me</button>
+            <button className={styles.viewButton}>View Projects</button>
+          </div>
+        </div>
+        <div className={styles.right}>
+          <div className={styles.imageContainer}>
+            {/* Insert your image or animation here */}
+            <img className={styles.logo} src="src/assets/images/home-img.png" alt="Logo" />
+          </div>
+        </div>
+      </section>
+    </main>
   );
 };
 
